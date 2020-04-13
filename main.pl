795869e078acc8c31a769250de5bd4f49bc04beb
KornSiwat <siwat@siwat.dev>
2020-04-13 20:04:25 +0700

    fix based on lecturer comment

--- main.pl
+++ main.pl
@@ -1,18 +1,22 @@
 :- use_module(rules,[]).
 
-list_wine():- forall(facts:wine(X), writeln(wine:X)).
-list_wineType():- forall(facts:wineType(X), writeln(wineType:X)).
-list_wine_name_type:- forall(rules:wine_in_type(NAME, TYPE), writeln(name:NAME+type:TYPE)).
-list_ingredient():- forall(facts:ingredient(X), writeln(ingredient:X)).
-list_ingredient_minor_type():- forall(facts:minorType(X), writeln(ingredientMinorType:X)).
-list_ingredient_major_type():- forall(facts:majorType(X), writeln(ingredientMajorType:X)).
-list_ingredient_name_minor_type():- forall(rules:ingredient_in_minor_type(X, Y), writeln(ingredient:X+type:Y)).
-find_ingredient_by_minor_type(Y):- forall(rules:ingredient_in_minor_type(X, Y), writeln(ingredient:X)).
-find_ingredient_by_major_type(Y):- forall(rules:ingredient_in_major_type(X, Y), writeln(ingredient:X)).
-list_ingredient_compatible_with_wine():- forall(rules:ingredient_compatible_with_wine(X, Y), writeln(ingredient:X+wine:Y)).
-find_compatible_ingredient_by_wine(Y):- forall(rules:ingredient_compatible_with_wine(X, Y), writeln(ingredient:X)).
-find_compatible_wine_by_ingredient(X):- forall(rules:ingredient_compatible_with_wine(X, Y), writeln(wine:Y)).
-list_ingredient_compatible_with_wine_type():- forall(rules:ingredient_compatible_with_wine_type(INGREDIENT_NAME, WINE_TYPE), writeln(ingredient:INGREDIENT_NAME+wineType:WINE_TYPE)).
-find_compatible_wine_type_by_ingredient(X):- forall(rules:ingredient_compatible_with_wine_type(X, Y), writeln(wineType:Y)).
-find_compatible_wine_type_by_ingredient(Y):- forall(rules:ingredient_compatible_with_wine_type(X, Y), writeln(ingredient:X)).
+wines:- forall(facts:wine(X), writeln(wine:X)).
+wineTypes:- forall(facts:wineType(X), writeln(wineType:X)).
+wineNameType:- forall(rules:wine_in_type(NAME, TYPE), writeln(name:NAME+type:TYPE)).
 
+ingredients:- forall(facts:ingredient(X), writeln(ingredient:X)).
+ingredientMinorTypes:- forall(facts:minorType(X), writeln(ingredientMinorType:X)).
+ingredientMajorTypes:- forall(facts:majorType(X), writeln(ingredientMajorType:X)).
+ingredientNameMinorTypes:- forall(rules:hasMinorType(X, Y), writeln(ingredient:X+type:Y)).
+ingredientNameMajorTypes:- forall(rules:hasMajorType(X, Y), writeln(ingredient:X+type:Y)).
+
+ingredientWineCompatiblePairs:- forall(rules:ingredientCompatibleWithWine(X, Y, Z), writeln(ingredient:X+wine:Y)).
+ingredientWineTypeCompatiblePairs:- forall(rules:ingredientCompatibleWithWine(X, Y, Z), writeln(ingredient:X+wineType:Y)).
+
+compatibleIngredientForWine(Y):- forall(rules:ingredientCompatibleWithWine(X, Y, Z), writeln(ingredient:X)).
+compatibleIngredientWithMajorTypeForWine(Z, Y):- forall(rules:ingredientCompatibleWithWine(X, Y, Z), writeln(ingredient:X)).
+compatibleWineForIngredient(X):- forall(rules:ingredientCompatibleWithWine(X, Y, Z), writeln(wine:Y)).
+
+compatibleWineTypeForIngredient(X):- forall(rules:ingredientCompatibleWithWineType(X, Y, Z), writeln(wineType:Y)).
+compatibleIngredientForWineType(Y):- forall(rules:ingredientCompatibleWithWineType(X, Y, Z), writeln(ingredient:X)).
+compatibleIngredientWithMajorTypeForWineType(Z, Y):- forall(rules:ingredientCompatibleWithWineType(X, Y, Z), writeln(ingredient:X)).
--- rules.pl
+++ rules.pl
@@ -1,33 +1,19 @@
 :- module(rules,[]).
 :- use_module(facts,[]).
 
-wine_in_type(NAME, TYPE):-
-  facts:wine(NAME),
-  facts:wineType(TYPE),
-  facts:isWineType(NAME, TYPE).
-
-ingredient_in_minor_type(NAME, TYPE):-
-  facts:ingredient(NAME),
-  facts:minorType(TYPE),
-  facts:hasMinorType(NAME, TYPE).
-
-ingredient_in_major_type(NAME, MAJOR_TYPE):-
-  facts:ingredient(NAME),
-  facts:minorType(MINOR_TYPE),
+hasMajorType(NAME, MAJOR_TYPE):-
   facts:hasMinorType(NAME, MINOR_TYPE),
-  facts:majorType(MAJOR_TYPE),
   facts:hasMajorType(MINOR_TYPE, MAJOR_TYPE).
 
-ingredient_compatible_with_wine(INGREDIENT_NAME, WINE_NAME):-
+ingredientCompatibleWithWine(INGREDIENT_NAME, WINE_NAME, MAJOR_TYPE):-
   facts:ingredient(INGREDIENT_NAME),
-  facts:wine(WINE_NAME),
-  facts:wineType(WINE_TYPE),
-  facts:isWineType(WINE_NAME, WINE_TYPE),
-  ingredient_in_minor_type(INGREDIENT_NAME, INGREDIENT_TYPE),
-  facts:compatible(INGREDIENT_TYPE, WINE_TYPE).
+  facts:hasMinorType(INGREDIENT_NAME, MINOR_TYPE),
+  hasMajorType(MINOR_TYPE, MAJOR_TYPE),
+  facts:compatible(MINOR_TYPE, WINE_TYPE),
+  facts:isWineType(WINE_NAME, WINE_TYPE).
 
-ingredient_compatible_with_wine_type(INGREDIENT_NAME, WINE_TYPE):-
+ingredientCompatibleWithWineType(INGREDIENT_NAME, WINE_TYPE, MAJOR_TYPE):-
   facts:ingredient(INGREDIENT_NAME),
-  facts:wineType(WINE_TYPE),
-  ingredient_in_minor_type(INGREDIENT_NAME, INGREDIENT_TYPE),
-  facts:compatible(INGREDIENT_TYPE, WINE_TYPE).
+  facts:hasMinorType(INGREDIENT_NAME, MINOR_TYPE),
+  hasMajorType(INGREDIENT_NAME, MAJOR_TYPE),
+  facts:compatible(MINOR_TYPE, WINE_TYPE).
