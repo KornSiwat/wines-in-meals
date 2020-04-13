126baf93cf009bb06e43e7baec688c05ccea9167
KornSiwat <siwat@siwat.dev>
2020-04-13 14:07:07 +0700

    update main

--- main.pl
+++ main.pl
@@ -6,6 +6,11 @@ list_wine_name_type:- forall(rules:wine_in_type(NAME, TYPE), writeln(name:NAME+t
 list_ingredient():- forall(facts:ingredient(X), writeln(ingredient:X)).
 list_ingredient_minor_type():- forall(facts:minorType(X), writeln(ingredientMinorType:X)).
 list_ingredient_major_type():- forall(facts:majorType(X), writeln(ingredientMajorType:X)).
+list_ingredient_name_minor_type():- forall(rules:ingredient_in_minor_type(X, Y), writeln(ingredient:X+type:Y)).
+list_ingredient_name_types():- forall(rules:ingredient_in_minor_type(X, Y), rules:ingredient_in_major_type(X, Z), writeln(ingredient:X+minorType:Y+majorType:Z)).
+find_ingredient_type(X):- forall(rules:ingredient_in_minor_type(X, Y), rules:ingredient_in_major_type(X, Z), writeln(minorType:Y+majorType:Z)).
+find_ingredient_by_minor_type(Y):- forall(rules:ingredient_in_minor_type(X, Y), writeln(ingredient:X)).
+find_ingredient_by_major_type(Y):- forall(rules:ingredient_in_major_type(X, Y), writeln(ingredient:X)).
 list_ingredient_compatible_with_wine():- forall(rules:ingredient_compatible_with_wine(X, Y), writeln(ingredient:X+wine:Y)).
 find_compatible_ingredient_by_wine(Y):- forall(rules:ingredient_compatible_with_wine(X, Y), writeln(ingredient:X)).
 find_compatible_wine_by_ingredient(X):- forall(rules:ingredient_compatible_with_wine(X, Y), writeln(wine:Y)).
